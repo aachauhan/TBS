02c12c849dc4be2c3772a7b746c2ba563edd8b71
Aditya <chauhan.adie@gmail.com>
2018-06-18 16:08:25 -0600

    after work 06/18
    got the html working for the svg information box

--- tbs.js
+++ tbs.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     var clickFlag = true;
 
     //tooltip for active pods style
-    var tooltip_1 = d3.select("body")
+    var tooltip_activePod = d3.select("body")
     .append("div")
     .style("position", "absolute")
     .style("z-index", "999")
     .style("visibility", "hidden")
     .style("width", "150px")
-    .style("height", "100px")
+    .style("height", "auto")
     .style("background", "#000")
     .style("top", "15vh")
     .style("left", "10%")
@@ -23,17 +23,19 @@ $(document).ready(function () {
     .style("z-index", "999")
     .style("visibility", "hidden")
     .style("width", "150px")
-    .style("height", "100px")
+    .style("height", "auto")
     .style("background", "#828282")
     .style("top", "15vh")
     .style("left", "10%")
     .style("border", "0.5px solid #efefef")
-    .style("padding-left", "10px");
+    .style("padding-left", "10px")
+    .style("padding-right", "10px");
 
     //logic for the inactive pods
     d3.selectAll(".cls-1").on('mouseover', function(d) {
         console.log("MouseOver");
-        tooltip.text("POD information coming soon!");   //text inside the div ~ tooltip
+        tooltip.text("POD information coming soon! Status Inactive.");   //text inside the div ~ tooltip
+        tooltip.html("<p>Inactive</p><hr /><a href='#' style='color:white;'>Read More...</a>");     //the html test
         tooltip.style("color", "white");    //style for the tooltip
         d3.select(this).style("fill", "#22b0ed");   //style for the pod
         return tooltip.style("visibility", "visible");  //shows the information box
@@ -65,62 +67,62 @@ $(document).ready(function () {
 
     //logic for the active pods - content
     d3.selectAll(".content").on('mouseover', function(d) {
-        tooltip_1.text("Content POD");   //text inside the div ~ tooltip
-        tooltip_1.style("color", "white");    //style for the tooltip
+        tooltip_activePod.text("Content POD");   //text inside the div ~ tooltip
+        tooltip_activePod.style("color", "white");    //style for the tooltip
         d3.select(this).style("fill", "22b0ed");   //style for the pod
-        return tooltip_1.style("visibility", "visible");  //shows the information bo
+        return tooltip_activePod.style("visibility", "visible");  //shows the information bo
     })
     .on('mouseout', function(){
         d3.select(this).style("fill", "steelblue");
-        return tooltip_1.style("visibility", "hidden");
+        return tooltip_activePod.style("visibility", "hidden");
     });
 
     //logic for the active pods - web
     d3.selectAll(".web").on('mouseover', function(d) {
-        tooltip_1.text("Web POD");   //text inside the div ~ tooltip
-        tooltip_1.style("color", "white");    //style for the tooltip
+        tooltip_activePod.text("Web POD");   //text inside the div ~ tooltip
+        tooltip_activePod.style("color", "white");    //style for the tooltip
         d3.select(this).style("fill", "#22b0ed");   //style for the pod
-        return tooltip_1.style("visibility", "visible");  //shows the information bo
+        return tooltip_activePod.style("visibility", "visible");  //shows the information box
     })
     .on('mouseout', function(){
         d3.select(this).style("fill", "#bdb530");
-        return tooltip_1.style("visibility", "hidden");
+        return tooltip_activePod.style("visibility", "hidden");
     });
 
     //logic for the active pods - design
     d3.selectAll(".design").on('mouseover', function(d) {
-        tooltip_1.text("Design POD");   //text inside the div ~ tooltip
-        tooltip_1.style("color", "white");    //style for the tooltip
+        tooltip_activePod.text("Design POD");   //text inside the div ~ tooltip
+        tooltip_activePod.style("color", "white");    //style for the tooltip
         d3.select(this).style("fill", "#22b0ed");   //style for the pod
-        return tooltip_1.style("visibility", "visible");  //shows the information bo
+        return tooltip_activePod.style("visibility", "visible");  //shows the information box
     })
     .on('mouseout', function(){
         d3.select(this).style("fill", "red");
-        return tooltip_1.style("visibility", "hidden");
+        return tooltip_activePod.style("visibility", "hidden");
     });
 
     //logic for the active pods - strategy
     d3.selectAll(".strategy").on('mouseover', function(d) {
-        tooltip_1.text("Strategy POD");   //text inside the div ~ tooltip
-        tooltip_1.style("color", "white");    //style for the tooltip
+        tooltip_activePod.text("Strategy POD");   //text inside the div ~ tooltip
+        tooltip_activePod.style("color", "white");    //style for the tooltip
         d3.select(this).style("fill", "#22b0ed");   //style for the pod
-        return tooltip_1.style("visibility", "visible");  //shows the information bo
+        return tooltip_activePod.style("visibility", "visible");  //shows the information box
     })
     .on('mouseout', function(){
         d3.select(this).style("fill", "#2a5365");
-        return tooltip_1.style("visibility", "hidden");
+        return tooltip_activePod.style("visibility", "hidden");
     });
 
     //logic for the active pods - seo
     d3.selectAll(".seo").on('mouseover', function(d) {
-        tooltip_1.text("SEO POD");   //text inside the div ~ tooltip
-        tooltip_1.style("color", "white");    //style for the tooltip
+        tooltip_activePod.text("SEO POD");   //text inside the div ~ tooltip
+        tooltip_activePod.style("color", "white");    //style for the tooltip
         d3.select(this).style("fill", "#22b0ed");   //style for the pod
-        return tooltip_1.style("visibility", "visible");  //shows the information bo
+        return tooltip_activePod.style("visibility", "visible");  //shows the information box
     })
     .on('mouseout', function(){
         d3.select(this).style("fill", "gray");
-        return tooltip_1.style("visibility", "hidden");
+        return tooltip_activePod.style("visibility", "hidden");
     });
 
     d3.selectAll(".cls-1").style("fill", "#efefef");
